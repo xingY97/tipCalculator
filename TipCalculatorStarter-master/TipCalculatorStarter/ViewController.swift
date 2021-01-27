90a8fa1f4aa9ac771ef0a1644ddc08f0a7c94338
xingY97 <45300300+xingY97@users.noreply.github.com>
2021-01-27 17:37:57 -0500

    finished auto layout for header, inputView, outputView, reset button

---
+++ TipCalculatorStarter-master/TipCalculatorStarter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculatorStarter
+//
+//  Created by Chase Wang on 9/19/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
